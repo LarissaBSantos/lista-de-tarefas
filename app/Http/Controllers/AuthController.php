6e59539ccf8b34374f75c23ccf42d6a144544289
Larissa Batista dos Santos <larissabatista0704@gmail.com>
2026-01-10 12:04:01 -0300

    feat(auth): adiciona controller de autenticação para registro e login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Log;
+use App\Models\User;
+
+class AuthController extends Controller
+{ 
+    public function Autenticar(Request $request): RedirectResponse 
+    {
+        $validacao = $request->validate([
+            'email' => ['required', 'email'],
+            'senha' => ['required'],
+        ]);
+        
+        $credenciais = ['email' => $request->email, 'password' => $request->senha];
+
+        if(Auth::attempt($credenciais)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email ou senha errado.'
+        ])->onlyInput('email');
+    }
+
+    public function CriarConta(Request $request): RedirectResponse
+    {
+        $validacao = $request->validate([
+            'nome' =>['required'],
+            'email' => ['required', 'unique:users,email', 'email'],
+            'senha' => ['required', 'min:4', 'confirmed'],
+        ],
+        [
+            'nome.required' => 'O campo nome é obrigatório.', 
+            'email.required' => 'O campo email é obrigatório.',
+            'email.unique' => 'Este email já está em uso.',
+            'email.email' => 'Formato de email inválido.',
+            'senha.required' => 'O campo senha é obrigatório.',
+            'senha.min' => 'A senha deve ter no mínimo 4 caracteres.',
+            'senha.confirmed' => 'A senha não coincide com repetir senha',
+        ]);
+
+        if($validacao) {
+            $usuario = User::create([
+                'name' => $request->nome,
+                'email' => $request->email,
+                'password' => Hash::make($request->senha),
+            ]);
+        }
+
+        return redirect('/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
+
+Route::get('/home', function () {
+    return view('pages.home');
+});
+
+Route::get('/login', function () {
+    return view('pages.login');
+});
+
+Route::get('/register', function () {
+    return view('pages.register');
+});
+
+Route::post('/register', [AuthController::class, 'CriarConta']);
+Route::post('/login', [AuthController::class, 'Autenticar']);
